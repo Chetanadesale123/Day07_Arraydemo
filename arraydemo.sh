c767f811e28df6807ec4174efc63a00d4ca8ab01
Chetanadesale123 <102363232+Chetanadesale123@users.noreply.github.com>
2022-03-28 18:17:45 +0530

    Update arraydemo.sh
    space changes

--- arraydemo.sh
+++ arraydemo.sh
@@ -3,11 +3,9 @@ subjects[0]="python";
 subjects[1]="java";
 subjects[2]="php";
 subjects[3]="core_java";
-
 echo "All elements of array : "${subjects[@]};
 echo "Size of array : " ${#subjects[@]};
 echo "printing element at index 1 :" ${subjects[1]};
-
 for ((i=0;i<${#subjects[@]};i++))
 do
    echo ${subjects[i]}
